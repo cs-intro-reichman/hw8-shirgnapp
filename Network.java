2721efc2e8829b04891b5d1614601b31d44653da
shirgnapp <gnapp3@gmail.com>
2025-01-10 12:53:01 +0200

    done

--- Network.java
+++ Network.java
@@ -28,58 +28,112 @@ public class Network {
     /** Finds in this network, and returns, the user that has the given name.
      *  If there is no such user, returns null.
      *  Notice that the method receives a String, and returns a User object. */
-    public User getUser(String name) {
+   // public User getUser(String name) {
+    //name = ChangeName(name);
+     //for(int i=0; i<this.users.length; i++){
+       // if(users[i].getName().equals(name))
+        //return users[i];
+    // }
+      //  return null;
+   // }
+   public User getUser(String name) {
     name = ChangeName(name);
-     for(int i=0; i<this.users.length; i++){
-        if(users[i].getName().equals(name))
-        return users[i];
-     }
-        return null;
+    for (int i = 0; i < users.length; i++) {
+        if (users[i] != null && users[i].getName().equals(name)) {
+            return users[i];
+        }
     }
-    
-    
+    return null; // אם המשתמש לא נמצא
+}
+
+  
     /** Adds a new user with the given name to this network.
     *  If ths network is full, does nothing and returns false;
     *  If the given name is already a user in this network, does nothing and returns false;
     *  Otherwise, creates a new user with the given name, adds the user to this network, and returns true. */
+   // public boolean addUser(String name) {
+     //   name = ChangeName(name);
+       // for(int i=0; i<this.users.length; i++){
+          //  if(users[i].getName().equals(name))
+            //return false;
+            //if(users[i].getName() == null){
+        //        users[i] = new User(name);
+          //      return true;
+           // }
+       // }
+       // return false;
+   // }
+    //public boolean addUser(String name) {
+        //name = ChangeName(name);
+    
+      //  if (getUser(name) != null) {
+     //       return false; // כבר קיים
+   //     }
+    
+        //if (userCount < users.length) {
+          //  users[userCount++] = new User(name);
+        //    return true;
+      //  }
+    //
+     //   return false; 
+   // }
     public boolean addUser(String name) {
         name = ChangeName(name);
-        for(int i=0; i<this.users.length; i++){
-            if(users[i].getName().equals(name))
+    
+        if (getUser(name) != null) {
             return false;
-            if(users[i].getName() == null){
-                users[i] = new User(name);
-                return true;
-            }
         }
-        return false;
-    }
-    public boolean IsExist(String name){
-        name = ChangeName(name);
-        for(int i=0; i<this.users.length; i++){
-            if (this.users[i].getName().equals(name))
+    
+        if (userCount < users.length) {
+            users[userCount++] = new User(name);
             return true;
-            }
-        return false;
         }
+    
+        return false; 
+    }
+    
 
     /** Makes the user with name1 follow the user with name2. If successful, returns true.
      *  If any of the two names is not a user in this network,
      *  or if the "follows" addition failed for some reason, returns false. */
-    public boolean addFollowee(String name1, String name2) {
-        name1 = ChangeName(name1);
-        name2 = ChangeName(name2);
-      if (IsExist(name1) == false || IsExist(name2)) {
-        return false;
-      }
-      User user1 = getUser(name1);
-      User user2 = getUser(name2);
-    if (user2.addFollowee(name1) && user1.addFollowee(name2)) {
-        return true;
+    //public boolean addFollowee(String name1, String name2) {
+        //name1 = ChangeName(name1);
+        //name2 = ChangeName(name2);
+      //if (IsExist(name1) == false || IsExist(name2)) {
+        //return false;
+      //}
+      //User user1 = getUser(name1);
+      //User user2 = getUser(name2);
+    //if (user2.addFollowee(name1) && user1.addFollowee(name2)) {
+      //  return true;
+    //}
+    //return false;
+   // }
+   public boolean addFollowee(String name1, String name2) {
+    name1 = ChangeName(name1);
+    name2 = ChangeName(name2);
+    User user1 = getUser(name1);
+    User user2 = getUser(name2);
+    if (user1 == null || user2 == null) {
+        return false; // אחד המשתמשים לא קיים
     }
-    return false;
-    }
-   
+    return user1.addFollowee(name2);
+}
+
+    //public boolean addFollowee(String name1, String name2) {
+      //  name1 = ChangeName(name1);
+       // name2 = ChangeName(name2);
+    //
+      //  User user1 = getUser(name1);
+        //User user2 = getUser(name2);
+    
+   //     if (user1 == null || user2 == null) {
+     //       return false; 
+      //  }
+    //
+      //  return user1.addFollowee(name2);
+    //}
+    
     /** For the user with the given name, recommends another user to follow. The recommended user is
      *  the user that has the maximal mutual number of followees as the user with the given name. */
     public String recommendWhoToFollow(String name) {
@@ -157,4 +211,14 @@ public class Network {
         name = First + name.substring(1); 
         return name;
     }
+    public boolean IsExist(String name) {
+        name = ChangeName(name); 
+        for (int i = 0; i < users.length; i++) {
+            if (users[i] != null && users[i].getName().equals(name)) {
+                return true; 
+            }
+        }
+        return false; 
+    }
+     
 }
