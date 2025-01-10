fb3cbd60a2872986017b49c22f663ba6059af2a2
shirgnapp <gnapp3@gmail.com>
2025-01-10 15:10:57 +0200

    done

--- User.java
+++ User.java
@@ -51,123 +51,37 @@ import java.time.chrono.IsoEra;
         }
         return false;
     }
-    
-    /** If this user follows the given name, returns true; otherwise returns false. */
-    //public boolean follows(String name) {
-      //  name = ChangeName(name);
-      // for(int i=0; i<10; i++){
-       // if (this.follows[i].equals(name)){
-        //  return true;
-       // }
-      // }
-        //return false;
-   // }
-    /** Makes this user follow the given name. If successful, returns true. 
-     *  If this user already follows the given name, or if the follows list is full, does nothing and returns false; */
-    //public boolean addFollowee(String name) {
-      //  if (name == null) {
-        //    return false; 
-        //}
-    
-        //name = ChangeName(name);
-    
-   //     if (this.name.equals(name)) { 
-     //       return false; 
-      //  }
-    
-        //for (int i = 0; i < fCount; i++) {
-          //  if (follows[i] != null && follows[i].equals(name)) { 
-            //    return false; 
-            //}
-       // }
-    
-       // if (fCount < maxfCount) { 
-         //   follows[fCount++] = name; 
-           // return true; 
-       // }
-    
-        //return false; 
-   // }
-
-    //public boolean addFollowee(String name) {
-
-      //  name = ChangeName(name);
-        //for (int i = 0; i < fCount; i++) {
-          //  if (follows[i].equalsIgnoreCase(name)) { 
-            //    return false; 
-           // }
-       // }
-
-        //if (fCount == maxfCount) { 
-          //  return false; 
-       // }
-       // follows[fCount++] = name; 
-       // return true; 
-      
-   // }
-
-   //public boolean addFollowee(String name) {
-   // if (name == null ) {
-     //   return false; 
-    //}
-  //  for (int i = 0; i < this.follows.length; i++) {
-      //  if (follows[i] != null && follows[i].toLowerCase().equals(name.toLowerCase())) {
-        //    return false; 
-       // }
-   // }
-    //if (fCount >= follows.length) {
-      //  return false; 
-   // }
-
-   // follows[fCount] = name;
-   // fCount++;
-   // return true;
-//}
+    ublic boolean addFollowee(String name) {
+        if (fCount==maxfCount) return false;
+        if (follows(name)) return false;
+        if (name.charAt(0)>='a' && name.charAt(0)<='z' ) {
+            String newName = name.substring(0, 1).toUpperCase() + name.substring(1);
+            follows[fCount] = newName;
+        }
+        else follows[fCount] = name;
+        fCount++;
+        return true;
+    }
 //public boolean addFollowee(String name) {
-
-    //if (name == null || name.trim().isEmpty()) {
-        //System.out.println("Cannot follow a null or empty name.");
-      //  return false;
-    //}
-    //name = ChangeName(name);
-    //if (follows(name)) {
-       // System.out.println(this.name + " is already following " + name);
-      //  return false;
-    //}
-
-    //if (fCount >= maxfCount) {
-     //   System.out.println(this.name + " reached the maximal number of follows. Can't follow " + name);
-      //  return false;
+  //  name = ChangeName(name);
+   // if (name == null || name.trim().isEmpty()) {
+     //   System.out.println("Invalid followee name.");
+       // return false;
    // }
-
-   // follows[fCount] = name;
-   // fCount++;
-   // System.out.println("Successfully added " + name + " to " + this.name);
-  //  return true;
+   // if (fCount >= maxfCount) {
+     //   System.out.println(this.name + " reached the maximum followee count.");
+       // return false;
+   // }
+   // if (follows(name)) {
+     //   System.out.println(this.name + " is already following " + name);
+       // return false;
+   // }
+//
+  //  follows[fCount] = name;
+    //fCount++;
+    //System.out.println("Successfully added " + name + " to " + this.name);
+    //return true;
 //}
-public boolean addFollowee(String name) {
-    name = ChangeName(name);
-    if (name == null || name.trim().isEmpty()) {
-        System.out.println("Invalid followee name.");
-        return false;
-    }
-    if (fCount >= maxfCount) {
-        System.out.println(this.name + " reached the maximum followee count.");
-        return false;
-    }
-
-    if (follows(name)) {
-        System.out.println(this.name + " is already following " + name);
-        return false;
-    }
-
-    follows[fCount] = name;
-    fCount++;
-    System.out.println("Successfully added " + name + " to " + this.name);
-    return true;
-}
-
-
     /** Removes the given name from the follows list of this user. If successful, returns true.
      *  If the name is not in the list, does nothing and returns false. */
     public boolean removeFollowee(String name) {
@@ -188,46 +102,7 @@ public boolean addFollowee(String name) {
         return false;
     }
     
-    
-   // public boolean removeFollowee(String name) {
-     //   name = ChangeName(name);
-       // for(int i=0; i<10; i++){
-         //   if (this.follows[i].equals(name)){
-           //     this.fCount--;
-             //  if (i == 9) {
-               // this.follows[i] = null; 
-                //return true;
-               //}
-               //else{
-               //while (this.follows[i] != null && i < 9) {
-               // this.follows[i] = this.follows[i+1]; 
-               // this.follows[i] = null;
-               // i++;
-               // if (i == 9) {
-                 //   this.follows[i] = null; 
-                   // return true;
-                //}
-              // }
-              // }
-           // }
-       // }
-        //return false;
-   // }
-
-    /** Counts the number of users that both this user and the other user follow.
-    /*  Notice: This is the size of the intersection of the two follows lists. */
-    //public int countMutual(User other) {
-        //int count = 0;
-        //for(int i=0; i<10; i++) {
-            //for(int j=0; j<10; j++){
-             //   if (this.follows[i].equals(other.follows[j]) && this.follows[i] != null) {
-           //      count ++;   
-         //       }
-       //     }
-     //   }
-   //     return count;
- //   }
-
+   
     public int countMutual(User other) {
         int count = 0;
         for (int i = 0; i < fCount; i++) {
@@ -240,29 +115,6 @@ public boolean addFollowee(String name) {
         return count;
     }
     
-    /** Checks is this user is a friend of the other user.
-     *  (if two users follow each other, they are said to be "friends.") */
-   // public boolean isFriendOf(User other) {
-    //boolean isFriend1 = false;
-    //boolean isFriend2 = false;
-    //String name1 = other.getName();
-    //name1 = ChangeName(name1);
-    //String thisname = this.name;
-    //thisname = ChangeName(thisname);
-    //if (other.follows(thisname))
-      //  isFriend1 = true;
-    //for(int i=0; i<10; i++){
-      //    if (this.follows[i].equals(name1)){
-        //    isFriend2 = true;
-          //  }
-       // }
-    //if (isFriend1 && isFriend2) {
-      //return true;
-    //}
-      //  return false;
-   // }
-    /** Checks if this user is a friend of the other user.
- *  (if two users follow each other, they are said to be "friends.") */
     public boolean isFriendOf(User other) {
    
     if (!other.follows(this.name)) {
